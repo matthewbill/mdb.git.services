82e8aee8d1ed45eec19eae00da6f6ca94a40905b
matthewbill <matthewdbill@gmail.com>
2019-02-16 19:57:08 +0000

    split out setup method

--- src/git-service.js
+++ src/git-service.js
@@ -9,35 +9,42 @@ class GitService {
     self.repoHostService = repoHostService;
   }
 
-  async setupRepo(pathToRepo, repoName, repoDescription, repoOrg) {
-    const self = this;
-    try {
-      const remoteUrl = await self.repoHostService.createRepo(
-        repoName, repoDescription, repoOrg,
-      );
-      const remoteDetails = await self.init(pathToRepo, remoteUrl);
-      return remoteDetails;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async init(pathToRepo, remoteUrl) {
+  async init(pathToRepo) {
     // eslint-disable-next-line no-unused-vars
     const self = this;
     try {
       const isBare = 0;
       const repo = await NodeGit.Repository.init(pathToRepo, isBare);
-      console.log(repo);
-      const remote = await NodeGit.Remote.create(repo, 'remote', remoteUrl);
-      console.log(remote);
-      return { repo, remote };
+      return repo;
     } catch (error) {
       throw error;
     }
   }
 
-  async initialCommit(repo, remote, message) {
+  async createRemoteRepo(repoName, repoDescription, repoOrg) {
+    const self = this;
+    try {
+      const remoteUrl = await self.repoHostService.createRepo(
+        repoName, repoDescription, repoOrg,
+      );
+      return remoteUrl;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createRemote(repo, remoteUrl) {
+    // eslint-disable-next-line no-unused-vars
+    const self = this;
+    try {
+      const remote = await NodeGit.Remote.create(repo, 'remote', remoteUrl);
+      return remote;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async commitAll(repo, message) {
     const self = this;
     try {
       const index = await repo.refreshIndex();
@@ -48,7 +55,16 @@ class GitService {
         self.email);
       const committer = NodeGit.Signature.now(self.username, self.email);
       await repo.createCommit('HEAD', author, committer, message, oid, []);
-      remote.push(['refs/heads/master:refs/heads/master'], {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async push(remote) {
+    const self = this;
+    try {
+      await remote.push(['refs/heads/master:refs/heads/master'], {
         callbacks: {
           credentials() {
             return NodeGit.Cred.userpassPlaintextNew(
@@ -57,7 +73,6 @@ class GitService {
           },
         },
       });
-      return;
     } catch (error) {
       throw error;
     }
